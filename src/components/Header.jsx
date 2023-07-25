c0ab4af042590e1558a0f909ffac0e65ade20762
Tundephilps <tundephilps@gmail.com>
2023-07-25 20:22:46 +0100

    final commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import fintech from "../images/fintech.png";
+
 import { Link } from "react-router-dom";
 
 export const Header = () => {
@@ -98,31 +99,11 @@ export const Header = () => {
               <div className="p-5 bg-white border rounded shadow-sm">
                 <div className="flex items-center justify-between mb-4">
                   <div>
-                    <Link
-                      to="/"
-                      aria-label="Company"
-                      title="Company"
-                      className="inline-flex items-center"
-                    >
-                      <svg
-                        className="w-8 text-deep-purple-accent-400"
-                        viewBox="0 0 24 24"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeMiterlimit="10"
-                        stroke="currentColor"
-                        fill="none"
-                      >
-                        <rect x="3" y="1" width="7" height="12" />
-                        <rect x="3" y="17" width="7" height="6" />
-                        <rect x="14" y="1" width="7" height="6" />
-                        <rect x="14" y="11" width="7" height="12" />
-                      </svg>
-                      <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                        Company
-                      </span>
-                    </Link>
+                    <div className="flex items-center justify-start">
+                      <Link to="/Homepage" className="flex ml-2 md:mr-24">
+                        <img src={fintech} className="h-[2.5rem]" />
+                      </Link>
+                    </div>
                   </div>
                   <div>
                     <button
